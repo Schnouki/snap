2f5835528c071cc5ef37be3c64c01292db70b465
Alex Cai <t9cai@uwaterloo.ca>
2016-06-26 00:50:22 -0400

    Exports supported

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /* Snap 3.1.6 | (C) 2014-2015 Alexandre Dieulot et Alex Mattrick | Apache-2.0 */
 
-export { init, preload } from './snap';
+export { init, preload, supported } from './snap';
 export { on, off } from './callbacks';
 import './loading-indicator';
